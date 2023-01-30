4a916f2a293ad500e3a076f0ef123d49d51cee84
AhamedAliNishad <ahamedalinishad.j@syncfusion.com>
2023-01-30 12:03:42 +0530

    EntryIssue

---
+++ EntryStyle/EntryStyle/EntryStyle/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace EntryStyle;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
